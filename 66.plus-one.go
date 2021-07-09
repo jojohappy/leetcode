0cdd2740a6e29fcb6a109d657399bb55cb436daf
jojohappy <sarahdj0917@gmail.com>
2021-07-09 15:24:10 +0800

    Update
    Signed-off-by: jojohappy <sarahdj0917@gmail.com>

---
+++ 66.plus-one.go
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=66 lang=golang
+ *
+ * [66] Plus One
+ */
+package main
+
+import (
+	"fmt"
+)
+
+// @lc code=start
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
+	}
+	return append([]int{1}, digits...)
+}
+
+// @lc code=end
+
+func main() {
+	output := plusOne([]int{7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 6})
+	fmt.Println(output)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jojohappy/leetcode
+
+go 1.16
